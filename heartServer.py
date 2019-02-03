a7e1aed1515ba4f2a89b75bc764fa0e570cff88f
U-WIT\luih <luih@WIT45000398.wit.private>
2019-02-03 10:37:16 -0500

    Edit server, added sample json, added website for AR graph

--- heartServer.py
+++ heartServer.py
@@ -35,3 +35,33 @@ def hello():
 @app.route("/getGood")
 def getData():
     return send_file('output.txt', as_attachment=True, attachment_filename='output.txt')
+
+# Serve ARjs
+@app.route("/arjs")
+def arjs():
+    return send_file('./ReactLiveChart/ARjs.html')
+
+# Serve data
+@app.route("/dat")
+def data():
+    read_file_path = "hrData.txt"
+    giantDataDump = []
+    with open(read_file_path, "r") as input_file:
+        for index, line in enumerate(input_file):
+            hr1 = float(line.split(' ')[0].rstrip())
+            hr2 = float(line.split(' ')[1].rstrip())
+            entry1 = {"x": index/10, "y": hr1, "z": 0, "size": 0.1, "color": "#ff0000"}
+            entry2 = {"x": index/10, "y": hr2, "z": 0, "size": 0.1, "color": "#00ff00"}
+            giantDataDump.append(entry1)
+            giantDataDump.append(entry2)
+            print(str(index/10) + " " + str(hr1) + " " + str(hr2))
+    input_file.close()
+    print("CLOSED INPUT FILE")
+    write_to_file_path = "./ReactLiveChart/dat.json"
+    output_file = open(write_to_file_path, "w+")
+    print("OPENED OUTPUT FILE")
+    print(str(giantDataDump))
+    output_file.write(str(giantDataDump))
+    print("Wrote TO OUTPUT FILE")
+    output_file.close()
+    return send_file('./ReactLiveChart/dat.json')
