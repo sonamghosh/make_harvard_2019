d00c0453d13625a19c8f08d47b000d2e646e21f7
Sonam Ghosh <sonamghosh@gmail.com>
2019-02-03 00:46:27 -0500

    saves to dataset dir the raw stuff

--- wheel_data.py
+++ wheel_data.py
@@ -4,10 +4,23 @@ import flask_server_data_query as fs
 from pathlib import Path
 import pickle
 import wget
+import os
 
+# Grabs Data
 data = requests.get('http://carboi.serveo.net/getGood')
 print(data)
 print(data.content)
 
+raw_dir = Path('dataset', 'driverheart', 'raw')
+raw_dir.mkdir(parents=True, exist_ok=True)
 
-filename = wget.download('http://carboi.serveo.net/getGood')
+
+# Downloads File
+def get_data(save=True):
+    if save:
+        filename = wget.download('http://carboi.serveo.net/getGood')
+        os.rename('./output.txt', './dataset/driverheart/raw/output.txt')
+    else:
+        pass
+
+get_data(save=True)
