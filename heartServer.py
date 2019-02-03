e0547acac06a2fae8ff3ded4ab1a1c781b9e5387
U-WIT\luih <luih@WIT45000398.wit.private>
2019-02-03 11:45:21 -0500

    Added updated files

--- heartServer.py
+++ heartServer.py
@@ -2,6 +2,7 @@ import pymongo
 import signal
 import sys
 import datetime
+import json
 from pymongo import MongoClient
 from flask import Flask, request, send_file
 app = Flask(__name__)
@@ -42,7 +43,7 @@ def arjs():
     return send_file('./ReactLiveChart/ARjs.html')
 
 # Serve data
-@app.route("/dat")
+@app.route("/datas")
 def data():
     read_file_path = "hrData.txt"
     giantDataDump = []
@@ -50,18 +51,18 @@ def data():
         for index, line in enumerate(input_file):
             hr1 = float(line.split(' ')[0].rstrip())
             hr2 = float(line.split(' ')[1].rstrip())
-            entry1 = {"x": index/10, "y": hr1, "z": 0, "size": 0.1, "color": "#ff0000"}
-            entry2 = {"x": index/10, "y": hr2, "z": 0, "size": 0.1, "color": "#00ff00"}
+            entry1 = {"x": index, "y": hr1, "z": 0.1, "size": 0.1, "color": "#ff0000"}
+            entry2 = {"x": index, "y": hr2, "z": 0.1, "size": 0.1, "color": "#00ff00"}
             giantDataDump.append(entry1)
             giantDataDump.append(entry2)
             print(str(index/10) + " " + str(hr1) + " " + str(hr2))
     input_file.close()
     print("CLOSED INPUT FILE")
-    write_to_file_path = "./ReactLiveChart/dat.json"
+    write_to_file_path = "./ReactLiveChart/datas.json"
     output_file = open(write_to_file_path, "w+")
     print("OPENED OUTPUT FILE")
     print(str(giantDataDump))
-    output_file.write(str(giantDataDump))
+    output_file.write(json.dumps(giantDataDump))
     print("Wrote TO OUTPUT FILE")
     output_file.close()
-    return send_file('./ReactLiveChart/dat.json')
+    return send_file('./ReactLiveChart/datas.json')
